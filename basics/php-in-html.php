a749636f78de02bd15d1404c79a59b1166b31922
babaibrahimov <2295401@student.edu.az>
2023-03-07 23:42:04 +0400

    Php Lessons V4

---
+++ basics/php-in-html.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Php in HTML</title>
+    </head>
+    <body>
+        <h1>Home Page</h1>
+        <?php 
+            $user = "babaibrahimovv";
+        ?>
+        <p>Hello, <a href="#"><?php echo $user;?></a></p>
+    </body>
+</html>
