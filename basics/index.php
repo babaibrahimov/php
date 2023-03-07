42c990ed11b3591165546e11e0e64dfe0b5a294d
babaibrahimov <2295401@student.edu.az>
2023-03-07 21:45:22 +0400

    Learn php lesson 0.1

---
+++ basics/index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+        <?php
+        
+        echo "Hello, World!";
+        print "Hello, World<br>";
+
+        $string= "Hello, World!"; //if string - ""
+        $number = 23; // if number don't use ""
+        $float = 15.43783; // double or float, don't use string
+
+        $x = 15;
+        $y = 5;
+
+        echo "x = ", $x, "<br>";
+        echo "y = ", $y, "<br>";
+
+        echo "x + y = ", $x + $y , "<br>";
+        echo "x - y = ", $x - $y , "<br>";
+        echo "x * y = ", $x * $y , "<br>";
+        echo "x / y = ", $x / $y , "<br>";
+        ?>
+    </body>
+</html>
