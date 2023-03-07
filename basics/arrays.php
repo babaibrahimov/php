0255d63c3d933f61c1b6513c20545a3aa6f20941
babaibrahimov <2295401@student.edu.az>
2023-03-07 23:50:39 +0400

    Php Lesson V5

---
+++ basics/arrays.php
@@ -0,0 +1,20 @@
+<?php
+
+$countries = array("Azerbaijan", "Turkey", "England", "United States", "Russia", "Georgia");
+
+echo $countries[0]."<br>";
+echo $countries[1]."<br>";
+echo $countries[2]."<br>";
+echo $countries[3]."<br>";
+echo $countries[4]."<br>";
+echo $countries[5]."<br>";
+
+$cities = array('First' => 'Baku', 'Second' => 'Ankara', 'Third' => 'London');
+
+echo "<br>";
+
+echo $cities['First']."<br>";
+echo $cities['Second']."<br>";
+echo $cities['Third']."<br>";
+
+?>
