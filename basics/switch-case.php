d15cfb881982a7bd4024769601f4c494f18f721c
babaibrahimov <2295401@student.edu.az>
2023-03-07 23:30:01 +0400

    Php Lesson V3

---
+++ basics/switch-case.php
@@ -0,0 +1,24 @@
+<?php
+
+//Switch Case
+
+$number = 1;
+
+switch($number){
+
+    case 1: //if number = 1 this line will work
+        echo "Case 1 will work";
+        break;
+    case 2: //if number = 2 this line will work
+        echo "Case 2 will work";
+        break;
+    case 3: //if number = 3 this line will work
+        echo "Case 3 will work";
+        break;
+
+
+}
+
+
+
+?>
