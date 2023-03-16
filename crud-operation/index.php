3b9ec289fa119f583279865eb27f3ce00f23217d
babaibrahimov <2295401@student.edu.az>
2023-03-16 15:16:59 +0400

    table

--- crud-operation/index.php
+++ crud-operation/index.php
@@ -30,6 +30,41 @@
                 <button type="submit" name="upload">Upload</button>
             </form>
         </div>
-    </center>    
+    </center> 
+    <!-- fetch data -->
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Delete</th>
+                    <th scope="col">Update</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                include 'config.php';
+                $pic = mysqli_query($con,"SELECT * FROM `tb_crud`");
+                while($row = mysqli_fetch_array($pic)){
+                echo "
+                    <tr>
+                        <td>$row[id]</td>
+                        <td>$row[name]</td>
+                        <td>$row[price]</td>
+                        <td><img src='$row[image]'  width = '200px'  height = '70px'></td>
+                        <td><a href='delete.php? id= $row[id]' class = 'btn btn-danger'>Delete</a></td>
+                        <td><a href='update.php? id= $row[id]' class = 'btn btn-danger'>Update</a></td>
+                        <td></td>      
+                    </tr>
+                    ";
+                }
+
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
