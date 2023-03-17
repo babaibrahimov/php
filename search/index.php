0bed453b9477b838a7468cb9cae23158bb2996b3
babaibrahimov <2295401@student.edu.az>
2023-03-17 20:09:18 +0400

    update index.php

--- search/index.php
+++ search/index.php
@@ -7,6 +7,40 @@
     <title>Live Search Area with PHP and Ajax</title>
 </head>
 <body>
+    <div class="search-area">
+        <label for="search">Search Data:</label>
+        <input type="text" name="search" placeholder="Search" id="search">
+    </div>
+    <hr>
+    <?php
     
+    include 'connect.php';
+    $smt=$conn->prepare("SELECT * FROM tb_search");
+    $smt->execute();
+    $result=$smt->get_result();
+    ?>
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>E-mail</th>
+                <th>Country</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while($row=$result->fetch_assoc()){?>
+            <tr>
+                <td><?= $row['id']; ?></td>
+                <td><?= $row['firstname']; ?></td>
+                <td><?= $row['surname']; ?></td>
+                <td><?= $row['email']; ?></td>
+                <td><?= $row['country']; ?></td>
+            </tr>
+            <?php }?>
+        </tbody>
+    </table>
+    <script src="./script.js"></script>
 </body>
 </html>
