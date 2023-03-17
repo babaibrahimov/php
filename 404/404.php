7decdc75c6d1531199b0f53451fccd804d5cc536
babaibrahimov <2295401@student.edu.az>
2023-03-17 21:52:10 +0400

    complete 404 project

--- 404/404.php
+++ 404/404.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 Page Not Found | Baba Ibrahimov</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="404.css">
 </head>
 <body>
     <div class="wrapper">
         <div class="textBx">
             <h1>4<span>0</span>4</h1>
             <h4>Page Not Found!</h4>
-            <a href="http://www.babaibrahimov.com">Home</a>
+            <a href="index.php">Home</a>
         </div>
         <div class="imgBx">
             <img src="https://raw.githubusercontent.com/babaibrahimov/404/master/img/404.png" alt="">
--- 404/index.php
+++ 404/index.php
@@ -4,19 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 Page Not Found | Baba Ibrahimov</title>
-    <link rel="stylesheet" href="./style.css">
+    <title>Home page</title>
 </head>
 <body>
-    <div class="wrapper">
-        <div class="textBx">
-            <h1>4<span>0</span>4</h1>
-            <h4>Page Not Found!</h4>
-            <a href="http://www.babaibrahimov.com">Home</a>
-        </div>
-        <div class="imgBx">
-            <img src="https://raw.githubusercontent.com/babaibrahimov/404/master/img/404.png" alt="">
-        </div>
-    </div>
+    <h1>Home</h1>
 </body>
 </html>
