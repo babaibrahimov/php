c4ad45ecaa959b9c5b54cb5df1f6f1b8a27548dc
babaibrahimov <2295401@student.edu.az>
2023-03-17 21:15:54 +0400

    table design

--- search/index.php
+++ search/index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Live Search Area with PHP and Ajax</title>
+    <link rel="stylesheet" href="style.css">
 
     <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> -->
 
