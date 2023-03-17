e436da33a0a9573df6da8223c01126d1da5bfe71
babaibrahimov <2295401@student.edu.az>
2023-03-17 21:41:49 +0400

    add 404.html

---
+++ 404/404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Pages Not Found</title>
+    <link rel="stylesheet" href="404.css">
+</head>
+<body>
+    <h1>Ooooopppsss.. Page Not Found</h1>
+</body>
+</html>
