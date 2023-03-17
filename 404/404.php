8605a6f39156f0625012c0b0500a906d9f587bc1
babaibrahimov <2295401@student.edu.az>
2023-03-17 21:50:26 +0400

    update 404

--- 404/404.php
+++ 404/404.php
@@ -4,10 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 Pages Not Found</title>
-    <link rel="stylesheet" href="404.css">
+    <title>404 Page Not Found | Baba Ibrahimov</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <h1>Ooooopppsss.. Page Not Found</h1>
+    <div class="wrapper">
+        <div class="textBx">
+            <h1>4<span>0</span>4</h1>
+            <h4>Page Not Found!</h4>
+            <a href="http://www.babaibrahimov.com">Home</a>
+        </div>
+        <div class="imgBx">
+            <img src="https://raw.githubusercontent.com/babaibrahimov/404/master/img/404.png" alt="">
+        </div>
+    </div>
 </body>
 </html>
