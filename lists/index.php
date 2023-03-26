711c9ed5962ff3d7e552eb3a72ba4cd283d968c2
babaibrahimov <2295401@student.edu.az>
2023-03-26 21:36:41 +0400

    basic html structure

---
+++ lists/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List</title>
+    <link rel="stylesheet" href="./assets/style.css">
+    <!-- ICON -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css" rel="stylesheet">
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="wrapper">
+        <div class="adding-area">
+            <form action="" method="post" autocomplete="off" enctype="multipart/form-data">
+                <input type="text" placeholder="Add Objects...">
+                <button type="submit">Add</button>
+            </form>
+        </div>
+        <div class="output-area">
+            <div class="boxes">
+                <div class="box">
+                    <a href="https://www.github.com" target="_blank">Github</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
